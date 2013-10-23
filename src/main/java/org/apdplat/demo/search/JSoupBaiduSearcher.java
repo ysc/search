016940d6febf7b39ea6203087ed0a3e0c9e11c26
ysc <yang-shangchuan@qq.com>
2013-10-23 23:09:05 +0800

    整理导入信息

--- src/main/java/org/apdplat/demo/search/JSoupBaiduSearcher.java
+++ src/main/java/org/apdplat/demo/search/JSoupBaiduSearcher.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
